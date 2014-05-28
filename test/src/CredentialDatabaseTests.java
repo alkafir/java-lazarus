c12c6c90afda506bce7ce819af28918b4820bf18
Alfredo Mungo <chimeranet89@gmail.com>
2014-05-28 22:00:15 +0200

    Added test structure

---
+++ test/src/CredentialDatabaseTests.java
@@ -0,0 +1,63 @@
+import wisedevil.test.annotation.*;
+import wisedevil.test.result.*;
+import static wisedevil.test.Assert.*;
+
+import java.util.Iterator;
+import wisedevil.credentials.*;
+
+@Name("CredentialDatabase methods")
+@ResultManager(ConsoleResultManager.class)
+public class CredentialDatabaseTests {
+	CredentialDatabase cd;
+	
+	public void setup() {
+		cd = new CredentialDatabase();
+		
+		Credential c1, c2, c3;
+		
+		c1 = new Credential("First");
+		c2 = new Credential("Second");
+		c3 = new Credential("Third");
+		
+		cd.add(c1);
+		cd.add(c2);
+		cd.add(c3);
+	}
+	
+	public void cleanup() {
+		cd = null;
+	}
+	
+	@Test
+	public void ismodified_test() {
+		assert cd.isModified();
+	}
+	
+	@Test
+	public void invalid_rename_test() {
+		Iterator<Credential> ci = cd.getCredentials().iterator();
+		Credential c1 = ci.next();
+		Credential c2 = ci.next();
+		
+		c1.setTitle(c2.getTitle());
+		
+		// Count
+		{
+			long cnt = cd.getCredentialsStream().filter(x -> ((Credential)x).getTitle().equals(c2.getTitle())).count();
+			assert cnt == 1: "Multiple item count is: " + cnt;
+		}
+		
+		// Count
+		{
+			String oldTitle = c1.getTitle();
+			
+			c1.setTitle("Ciccio");
+		
+			long cnt1 = cd.getCredentialsStream().filter(x -> ((Credential)x).getTitle().equals("Ciccio")).count();
+			long cnt2 = cd.getCredentialsStream().filter(x -> ((Credential)x).getTitle().equals(oldTitle)).count();
+			assert cnt1 == 1 && cnt2 == 0: "Renamed item count is: " + cnt1 + ", Old-named item count is: " + cnt2;
+			
+			c1.setTitle(oldTitle);
+		}
+	}
+}
