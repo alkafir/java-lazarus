05f06e546693f144c0b16c181623bac1c6142c13
Alfredo Mungo <chimeranet89@gmail.com>
2014-06-24 11:12:52 +0200

    Added WDC import data structures

---
+++ src/wisedevil/credentials/export/WDCImporter.java
@@ -0,0 +1,160 @@
+/*
+ * Lazarus: Credentials management library
+ *     Copyright (C) 2014 Alfredo 'wisedevil' Mungo
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package wisedevil.credentials.export;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import java.security.AlgorithmParameters;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.InvalidParameterException;
+import java.security.NoSuchAlgorithmException;
+
+import java.security.spec.InvalidParameterSpecException;
+
+import java.util.Arrays;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+
+import javax.security.auth.DestroyFailedException;
+
+import wisedevil.credentials.CredentialDatabase;
+import wisedevil.credentials.TextPassword;
+
+import static wisedevil.credentials.export.internal.WDCUtil.passToDigest;
+
+/**
+ * This class provides support for importing a CredentialDatabase from a
+ * "WiseDevil Credentials" file.
+ */
+public class WDCImporter implements Importer {
+	/**
+	 * The decryption password.
+	 */
+	private final TextPassword pass;
+	
+	/**
+	 * The encrypted data.
+	 */
+	private final WDCEncryptionRecord encdata;
+	
+	/**
+	 * Initializes a new instance of this class.
+	 *
+	 * @param data The encrypted data
+	 * @param pass The decryption password
+	 *
+	 * @throws NullPointerException If any of the arguments is null
+	 */
+	public WDCImporter(WDCEncryptionRecord data, TextPassword pass) {
+		if(data == null || pass == null)
+			throw new NullPointerException();
+		
+		this.pass = pass;
+		encdata = data;
+	}
+	
+	public void destroy() throws DestroyFailedException {
+		pass.destroy();
+	}
+	
+	public boolean isDestroyed() { return pass.isDestroyed(); }
+	
+	/**
+	 * Imports the credentials.
+	 *
+	 * @return The imported credentials as a CredentialDatabase object
+	 *
+	 * @throws DatabaseImportException if an exception occurs during the process
+	 */
+	public CredentialDatabase importDatabase() throws DatabaseImportException {
+		byte[] passdgst = null;
+		byte[] raw = null;
+		
+		try {
+			passdgst = passToDigest(pass.get());
+			raw = decryptDatabase(encdata, passdgst);
+			return deserializeDatabase(raw);
+			
+		} catch(Exception e) {
+			throw new DatabaseImportException(e);
+		} finally {
+			if(passdgst != null)
+				Arrays.fill(passdgst, (byte)0);
+			
+			if(raw != null)
+				Arrays.fill(raw, (byte)0);
+		}
+	}
+	
+	/**
+	 * Decrypts the database using AES/CBC/PKCS5Padding.
+	 *
+	 * @param enc The encrypted data to be decrypted
+	 * @param pass The decryption password (hash)
+	 *
+	 * @return The decrypted database as an array of bytes
+	 *
+	 * @see https://gist.github.com/wisedevil/47fd55226a7a4cbcf4c6
+	 */
+	private static byte[] decryptDatabase(WDCEncryptionRecord enc, byte[] pass)
+		throws	NoSuchAlgorithmException,
+			NoSuchPaddingException,
+			InvalidKeyException,
+			InvalidParameterException,
+			InvalidParameterSpecException,
+			InvalidAlgorithmParameterException,
+			IllegalBlockSizeException,
+			BadPaddingException {
+		Cipher c = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		SecretKey seckey = new SecretKeySpec(pass, 0, pass.length, "AES");
+		AlgorithmParameters parms = AlgorithmParameters.getInstance("AES");
+
+		// Init
+		parms.init(new IvParameterSpec(enc.getIV()));
+		c.init(Cipher.DECRYPT_MODE, seckey, parms);
+
+		// Decrypt
+		return c.doFinal(enc.getData());
+	}
+	
+	/**
+	 * Deserializes the credential database.
+	 *
+	 * @return The deserialized credential database as a byte array
+	 *
+	 * @throws IOException If an output exception occurs during the deserialization process
+	 */
+	private static CredentialDatabase deserializeDatabase(byte[] rawdb) throws IOException, ClassNotFoundException {
+		try (
+			ByteArrayInputStream bs = new ByteArrayInputStream(rawdb);
+			ObjectInputStream os = new ObjectInputStream(bs)
+		) {
+			return (CredentialDatabase)os.readObject();
+		}
+	}
+}
